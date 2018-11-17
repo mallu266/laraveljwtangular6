46a009172d52a433b9d225171d9a0751ea79eb00
Arjun <mallikarjun266@gmail.com>
2018-11-17 13:38:31 +0530

    LARAVELJWTANGULAR

---
+++ backend/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+
+Route::group([
+//    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('payload', 'AuthController@payload');
+
+});
